cf91d7042767e766d128c3f80a18778913927d3c
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-12-09 12:36:21 +0600

    first commit

--- index.js
+++ index.js
@@ -9,39 +9,24 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k3thtz2.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log("Database connected: ", uri );
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
   try {
-    const db = client.db("moontech");
-    const productCollection = db.collection("product");
+    const db = client.db("MoonTec");
+    const productCollection = db.collection("products");
 
-    app.get("/products", async (req, res) => {
+    app.get("/product", async (req, res) => {
+      console.log("Mon Tech welcome");
       const cursor = productCollection.find({});
       const product = await cursor.toArray();
 
       res.send({ status: true, data: product });
     });
 
-    app.post("/product", async (req, res) => {
-      const product = req.body;
-
-      const result = await productCollection.insertOne(product);
-
-      res.send(result);
-    });
-
-    app.delete("/product/:id", async (req, res) => {
-      const id = req.params.id;
-
-      const result = await productCollection.deleteOne({ _id: ObjectId(id) });
-      res.send(result);
-    });
   } finally {
   }
 };
